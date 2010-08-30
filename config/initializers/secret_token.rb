c29339d8c9e67a24a1564f55e9614e5f3e4a7065
seungjin <seungjin@email.arizona.edu>
2010-08-30 16:24:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DSeungjinNet::Application.config.secret_token = '913abbbea6cd43845d59d4dd6f1739d2d23dadbae87dd6088291f284a23f38f283ddd590c98b099e4636a64a0091681352dc1f9416fd3573f236e77382f9b56b'
